5fc9db5ed15c05a5cc8353ef73cd05e1581209b4
Christopher <clong35@msudenver.edu>
2024-11-19 18:56:32 -0700

    started working on model page

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 import csv
 
-app = Flask(__name__)
+# import numpy as np
+# import pandas as pd
+# from sklearn.linear_model import LinearRegression
+# from sklearn import Des
 
+app = Flask(__name__)
 CORS(app)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+selected_features = []
+selected_model = ""
+
+@app.route('/')
+def view_form():
+    return render_template('login.html', features=get_data())
+
+
 @app.route('/api/data', methods=['GET'])
 def get_data():
     data = []
@@ -25,7 +38,7 @@ def get_data():
                 "isFeatureSelected": True,
                 "formattedFeatureName": feature_names[i].title().replace('_', ' '),
             })
-
+    return data
     return jsonify(data)
 
     # with open("heart_failure_clinical_records_dataset.csv") as f:
@@ -36,6 +49,26 @@ def get_data():
     #         list_of_column_names.append(row)
     # data = {'message': list_of_column_names[0]}
     # return jsonify(data)
+
+
+@app.route('/api/set_data', methods=['GET', 'POST'])
+def set_data():
+
+    if request.method == 'POST':
+
+        print(request.form)
+        # selected_features = request.form.get('selected_features')
+        # selected_model = request.form['selected_model']
+        # print(selected_features, selected_model)
+        return "<h1>Welcome!!!</h1>"
+    return "<h1>Hello!!!</h1>"
+    
+    # return render_template('login.html')#"<h1>Welcome!!!</h1>"
+
+
+def train_model(features, model):
+    pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
